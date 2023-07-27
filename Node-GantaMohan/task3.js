59f11005cd6fc8652e61ae6065153113a4045be9
Mohan Ganta <gantamohan.556@gmail.com>
2023-07-27 12:20:29 +0530

    Task3.js

---
+++ Node-GantaMohan/task3.js
@@ -0,0 +1,31 @@
+
+const nodemailer = require('nodemailer');
+
+
+const emailConfig = {
+  service: 'gmail', // 'gmail'
+  auth: {
+    user: 'gantamohan.556@gmail.com',
+    pass: 'CSE@2003',
+  },
+};
+
+const transporter = nodemailer.createTransport(emailConfig);
+
+const mailOptions = {
+  from: 'gantamohan.556@gmail.com', 
+  to: 'pravin.consensus@gmail.com', 
+  subject: 'Test Email from Mohan Ganta..!!',
+  text: 'Hello, This Is Mohan Ganta!',
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.log('Error occurred while sending email:');
+    console.log(error.message);
+  } else {
+    console.log('Email sent successfully:');
+    console.log('Message ID: ', info.messageId);
+    console.log('Preview URL: ', nodemailer.getTestMessageUrl(info));
+  }
+});
